0e29489431f4c0a9e8100fb4cd25e4d455af2731
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-10-09 15:40:16 -0400

    Use alternat git syntax

--- prereqs/cert.tf
+++ prereqs/cert.tf
@@ -1,5 +1,6 @@
 module "tls_certs" {
-  source = "./terraform-acme-tls-azurerm"
+  # source = "./terraform-acme-tls-azurerm"
+  source = "git::ssh://github.com/hashicorp-services/terraform-acme-tls-azurerm.git?ref=8b72f32ac1cdf6af2cdcfbf9d84dae115cfa1b0e"
 
   dns_zone_name                = var.zone_name
   dns_zone_resource_group_name = var.resource_group_name
